81cfc5d081373beacea3c6cc9e9931ed0d62c159
Jignesh Patel <jnpatel42@students.tntech.edu>
2017-01-30 08:27:39 -0600

    part of the lab

--- ReadFile.h
+++ ReadFile.h
@@ -6,17 +6,20 @@
 #include <fstream>
 using namespace std;
 
-struct ReadFile
+class ReadFile
 {
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+  Private:
+     ifstream input_file;
+     bool _eof;
+     bool closed;
+  Public:
+    ReadFile(const char* file_name);
+    ~ReadFile(ReadFile* rf);
+    String* readLine(ReadFile* rf);
+    bool eof(ReadFile* rf);
+    void close(ReadFile* rf);
 };
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
+
 
 #endif
